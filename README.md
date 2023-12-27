7cd5b1974d8a487be50a14080582c083d9b84b05
André Cipriani Bandarra <andreban@gmail.com>
2023-12-27 17:41:27 +0000

    Add the GCS demo and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Vertex API / Gemini in Rust
+
+Aa port of the [Vertex API][4] Gemini [demos by Mete Atamel][1] to Rust.
+
+## Running the demo
+
+1. Enable the Vertex AI API in the [Cloud Console][2].
+2. Install the [Google Cloud SDK][3] is insalled and configured and that you are logged in into an account with the Vertex API enabled in the Cloud Console. You can use `gcloud auth login` to log in.
+3. Ensure you have the following environment variables set:
+  - `API_ENDPOINT`: The endpoint of the Vertex AI API.
+  - `PROJECT_ID`: The ID of the project to use.
+  - `LOCATION_ID`: The ID of the location to use (also called region).
+
+Alternatively, create `.cargo/config.toml` with the following content:
+```toml
+[env]
+API_ENDPOINT="<api endpoint>"
+PROJECT_ID="<project id>"
+LOCATION_ID="<location id (also called region)"
+```
+
+4. Then run the demo with `cargo run`.
+
+[1]: https://github.com/meteatamel/genai-samples/tree/main/vertexai/gemini/console/csharp/rest
+[2]: https://console.cloud.google.com/apis/library/aiplatform.googleapis.com?q=vertex
+[3]: https://cloud.google.com/sdk/?hl=en_US
+[4]: https://cloud.google.com/vertex-ai/docs
---
+++ src/bin/generate-text-from-image-gcs.rs
@@ -0,0 +1,65 @@
+use std::env;
+
+use gcp_auth::AuthenticationManager;
+use gemini_rust::{
+    Content, GenerateContentRequest, GenerateContentResponse, GenerationConfig, Part,
+};
+
+static MODEL_NAME: &str = "gemini-pro-vision";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_endpoint = env::var("API_ENDPOINT")?;
+    let project_id = env::var("PROJECT_ID")?;
+    let location_id = env::var("LOCATION_ID")?; // Sometimes called "region" in gCloud docs.
+
+    let endpoint_url = format!(
+        "https://{api_endpoint}/v1beta1/projects/{project_id}/locations/{location_id}/publishers/google/models/{MODEL_NAME}:streamGenerateContent"
+    );
+
+    let authentication_manager = AuthenticationManager::new().await?;
+    let scopes = &["https://www.googleapis.com/auth/cloud-platform"];
+    let token = authentication_manager.get_token(scopes).await?;
+
+    let prompt = "Describe this image";
+    let image_url = "gs://cloud-samples-data/generative-ai/image/320px-Felis_catus-cat_on_snow.jpg";
+
+    let payload = GenerateContentRequest {
+        contents: vec![Content {
+            role: "user".to_string(),
+            parts: vec![
+                Part::Text(prompt.to_string()),
+                Part::FileData {
+                    mime_type: "image/jpeg".to_string(),
+                    file_uri: image_url.to_string(),
+                },
+            ],
+        }],
+        generation_config: GenerationConfig {
+            max_output_tokens: 2048,
+            temperature: 0.4,
+            top_p: 1,
+            top_k: 32.0,
+        },
+    };
+
+    let resp = reqwest::Client::new()
+        .post(&endpoint_url)
+        .bearer_auth(token.as_str())
+        .json(&payload)
+        .send()
+        .await?;
+
+    let response = resp.json::<GenerateContentResponse>().await?;
+    response.0.iter().for_each(|chunk| {
+        chunk.candidates.iter().for_each(|candidate| {
+            candidate.content.parts.iter().for_each(|part| {
+                if let Part::Text(text) = part {
+                    print!("{}", text);
+                }
+            });
+        });
+    });
+
+    Ok(())
+}
